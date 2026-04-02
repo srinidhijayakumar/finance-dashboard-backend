59bdbb9c402d8098fb79ae7dbe49ad5c4df2c846
Srinidhi <srinidhi@Srinidhis-MacBook-Air.local>
2026-04-02 21:22:18 +0530

    login and registration function

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const protect = require("../middleware/authMiddleware");
+const authorize = require("../middleware/roleMiddleware");
 
+router.get("/admin", protect, authorize("admin"), (req, res) => {
+  res.send("Only admin access");
+});
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 
--- src/services/authService.js
+++ src/services/authService.js
@@ -2,36 +2,45 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-exports.register = async ({ name, email, password, role }) => {
+const register = async (data) => {
+  const { name, email, password, role } = data;
+
   const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    throw new Error("User already exists");
-  }
+  if (existingUser) throw new Error("User already exists");
+
   const hashedPassword = await bcrypt.hash(password, 10);
+
   const user = await User.create({
     name,
     email,
-    password: hashedPassword,
+    password: hashedPassword, // ✅ IMPORTANT
     role,
   });
 
   return user;
 };
+const login = async (data) => {
+  const { email, password } = data;
 
-exports.login = async ({ email, password }) => {
   const user = await User.findOne({ email });
-  if (!user) {
-    throw new Error("Invalid credentials");
-  }
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) {
-    throw new Error("Invalid credentials");
-  }
-  const token = jwt.sign(
-    { id: user._id, role: user.role },
-    process.env.JWT_SECRET,
-    { expiresIn: "1d" }
-  );
+  console.log("USER FROM DB:", user);
 
-  return { user, token };
+  if (!user) throw new Error("User not found");
+
+  console.log("Entered password:", password);
+  console.log("Stored password:", user.password);
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  console.log("Password match:", isMatch);
+
+  if (!isMatch) throw new Error("Invalid credentials");
+
+  return user;
+
+
+};
+
+module.exports = {
+  register,
+  login,
 };
